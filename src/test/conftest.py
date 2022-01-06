25f622bbe199ca4f9417e5e03ead22150dc10882
Javier <javierherrerorprieto@gmail.com>
2022-01-06 17:57:04 +0100

    Se ha añadido el coverage en coverageResults.txt

--- src/test/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-@pytest.fixture(autouse=True)
-def separador():
-    print("\nInicio de test de función\t----------------------------------\n")
-    yield
-    print("\nFin de test de función\t\t----------------------------------")
